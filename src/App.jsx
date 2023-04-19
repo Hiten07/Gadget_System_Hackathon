2f017a89b0d25441963b1a96c3d7170ff4ebbb5b
DeepMakhasana <deepmakhasana1013@gmail.com>
2023-04-19 09:36:31 +0530

    added Navigation bar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './pages/Home';
-import login from './pages/login';
-import register from './pages/register';
+import Login from './pages/login';
+import Register from './pages/register';
+import Navbar from './components/Navbar';
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/home' element={<Home/>} />
-        <Route path='/register' element={<Register/>} />
-        <Route path='/login' element={<Login/>} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/home' element={<Home />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import "./styles/navbar.css";
+
+function Navbar() {
+    return (
+        <nav className='navbar'>
+            <div className="logo">
+                <h1><Link to="/">MGS</Link></h1>
+            </div>
+            <ul className='navbar-link'>
+                <li><Link to="/register">Registration</Link></li>
+                <li><Link to="/register">Login</Link></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
